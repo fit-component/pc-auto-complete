1c65086f40563e1c75848a526868423853fa31a9
huangziyi01 <huangziyi01@baidu.com>
2016-02-18 11:30:24 +0800

    quick push

--- src/auto-complete/index.js
+++ src/auto-complete/index.js
@@ -17,7 +17,13 @@ export default class AutoComplete extends React.Component {
         this.state = {
             inputWidth: 0,
             showComplete: false,
+
+            // 预设或者是后端返回的全部datas
             datas: [],
+
+            // 去重后的datas
+            filterDatas: [],
+
             value: '',
 
             // 当前选中第几个
@@ -67,60 +73,88 @@ export default class AutoComplete extends React.Component {
                     method: this.props.method,
                     data: this.props.beforeSend(value)
                 }).done((res)=> {
+                    let datas = this.props.complete(res)
                     this.setState({
-                        datas: this.props.complete(res),
-                        selectIndex: -1
+                        datas: datas,
+                        filterDatas: this.filterDatas(datas),
+                        selectIndex: -1,
+                        showComplete: true
                     })
                 })
             }, this.props.delay)
         } else {
             this.setState({
                 datas: this.props.datas,
-                selectIndex: -1
+                filterDatas: this.filterDatas(this.props.datas),
+                selectIndex: -1,
+                showComplete: true
             })
         }
     }
 
-    handleSelect(text, value, index, close = true) {
+    filterDatas(datas) {
+        let newDatas = []
+
+        datas.map((item, index)=> {
+            let regex = reg(this.searchValue)
+            if (this.searchValue === '' || (this.props.autoFilter && !regex.test(item[this.props.parse.text]))) {
+                return
+            }
+
+            // 超过最大数量则取消显示
+            if (index + 1 > this.props.maxNumber) {
+                return
+            }
+
+            newDatas.push(item)
+        })
+
+        return newDatas
+    }
+
+    handleSelect(text, value, index, close = true, realSelect = false) {
         this.setState({
             value: text,
             selectIndex: index,
             showComplete: !close
         }, ()=> {
-            this.props.onSelect(value)
+            if (realSelect) {
+                this.props.onSelect(value)
+            }
         })
     }
 
     handleKeyDown(event) {
-        if (this.state.datas.length === 0) return
+        if (this.state.filterDatas.length === 0) return
 
         switch (event.keyCode) {
         case 40:
+            // 上
             let newUpIndex = this.state.selectIndex + 1
-            if (newUpIndex > this.state.datas.length - 1) {
+            if (newUpIndex > this.state.filterDatas.length - 1) {
                 newUpIndex = 0
             }
-            this.handleSelect(this.state.datas[newUpIndex][this.props.parse.text], this.state.datas[newUpIndex][this.props.parse.value], newUpIndex, false)
-            // 上
+            this.handleSelect(this.state.filterDatas[newUpIndex][this.props.parse.text], this.state.filterDatas[newUpIndex][this.props.parse.value], newUpIndex, false, false)
             break
         case 38:
+            // 下
             let newDownIndex = this.state.selectIndex - 1
             if (newDownIndex < 0) {
-                newDownIndex = this.state.datas.length - 1
+                newDownIndex = this.state.filterDatas.length - 1
             }
-            this.handleSelect(this.state.datas[newDownIndex], newDownIndex, false)
-            // 下
+            this.handleSelect(this.state.filterDatas[newDownIndex][this.props.parse.text], this.state.filterDatas[newDownIndex][this.props.parse.value], newDownIndex, false, false)
             break
+        case 13:
+            // enter
+            this.handleSelect(this.state.filterDatas[this.state.selectIndex][this.props.parse.text], this.state.filterDatas[this.state.selectIndex][this.props.parse.value], this.state.selectIndex, true, true)
         }
     }
 
     render() {
-        let completeContainerStyle = {
-            width: this.state.inputWidth,
-            display: this.state.showComplete ? 'block' : null
-        }
+        // 是否为空
+        let isEmpty = true
 
-        let Items = this.state.datas.map((item, index)=> {
+        let Items = this.state.filterDatas.map((item, index)=> {
             let itemClass = classNames({
                 'item': true,
                 'active': index === this.state.selectIndex
@@ -129,17 +163,20 @@ export default class AutoComplete extends React.Component {
             let regex = reg(this.searchValue)
             let matchedString = item[this.props.parse.text].replace(regex, '<span class="highlight">' + this.searchValue + '</span>')
 
-            if (this.searchValue === '' || (this.props.autoFilter && !regex.test(item[this.props.parse.text]))) {
-                return null
-            }
+            isEmpty = false
 
             return (
-                <div onClick={this.handleSelect.bind(this,item[this.props.parse.text],item[this.props.parse.value],index)}
+                <div onClick={this.handleSelect.bind(this,item[this.props.parse.text],item[this.props.parse.value],index,true,true)}
                      key={index}
                      className={itemClass}
                      dangerouslySetInnerHTML={{__html: matchedString}}></div>
             )
         })
+
+        let completeContainerStyle = {
+            width: this.state.inputWidth,
+            display: this.state.showComplete && !isEmpty ? 'block' : null
+        }
 
         return (
             <div className="_namespace">
